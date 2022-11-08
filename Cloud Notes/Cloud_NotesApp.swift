1ddd40b692552393282c13d60be658ca54ccbdea
Simran Preet Singh <simran5590@gmail.com>
2022-11-08 07:07:00 -0800

    Initial Commit

---
+++ Cloud Notes/Cloud_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cloud_NotesApp.swift
+//  Cloud Notes
+//
+//  Created by Simran Preet Narang on 2022-11-08.
+//
+
+import SwiftUI
+
+@main
+struct Cloud_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4588d939144e1783156f98c9703f9ca36fcf5c7d
Simran Preet Singh <simran5590@gmail.com>
2022-11-10 07:36:50 -0800

    Saving image into CloudKit

--- Cloud Notes/CRUDView.swift
+++ Cloud Notes/CRUDView.swift
@@ -101,22 +101,39 @@ class CRUDViewModel: ObservableObject {
         
         let record = CKRecord(recordType: "Fruit")
         record["name"] = name
+        
+        // Attaching Image
+        guard let image = UIImage(named: "twitter"),
+              let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent("\(name).jpg"),
+              let data = image.jpegData(compressionQuality: 1.0)
+        else { return }
+        
+        do {
+            try data.write(to: url)
+            let asset = CKAsset(fileURL: url)
+            record["image"] = asset
+        } catch {
+            print("❌ Error in \(#function) ", error)
+        }
+        
+        // Saving it.
         saveItem(record: record)
     }
     
     private func saveItem(record: CKRecord) {
         
-        CKContainer.default().privateCloudDatabase.save(record) { [weak self] record, error in
+        CKContainer.default().publicCloudDatabase.save(record) { [weak self] record, error in
             
             // TODO: Handle errors better
             print("Record: \(record)")
             print("❌ ERROR: \(error)")
             
-            DispatchQueue.main.async {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
                 self?.text = ""
+                self?.fetchItems()
             }
             
-            self?.fetchItems()
+            
         }
     }
     
@@ -160,7 +177,7 @@ class CRUDViewModel: ObservableObject {
         }
         
         // Add the Operation to the public cloud database
-        CKContainer.default().privateCloudDatabase.add(queryOperation)
+        CKContainer.default().publicCloudDatabase.add(queryOperation)
     }
     
     func updateItem(fruit: FruitModel) {
@@ -174,7 +191,7 @@ class CRUDViewModel: ObservableObject {
         let fruit = fruits[index]
         let record = fruit.record
         
-        CKContainer.default().privateCloudDatabase.delete(withRecordID: record.recordID) { id, error in
+        CKContainer.default().publicCloudDatabase.delete(withRecordID: record.recordID) { id, error in
             // TODO: Handle errors better
             print("Record: \(id)")
             print("❌ ERROR: \(error)")
